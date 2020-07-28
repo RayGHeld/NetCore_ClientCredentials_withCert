2d985439cd86b3ecca2773664b0b2ca0980b4ce7
Ray Held <40300226+RayGHeld@users.noreply.github.com>
2020-07-27 19:59:50 -0500

    Add files via upload

---
+++ Create_ClientCred_Cert.ps1
@@ -0,0 +1,25 @@
+﻿# Create self-signed certificate and export pfx and cer files
+
+# please be aware that running this script will create a new cert each time in your certificate store.  If you need to rerun it, I recommend deleting the previous certs first in the store.
+
+$tenant_id = 'rayheld.com' # tenant id is used for the cert name / subject name and matches the .netcore code sample that goes with this script
+$client_id = 'd48514a1-0d32-4a1d-8d26-f3edcad0d2a9' # client id is used for the cert name / subject name and cert password and matches the .netcore code sample that goes with this script
+$FilePath = 'C:\Users\raheld\OneDrive - Microsoft\Documents\Certs\'
+$StoreLocation = 'CurrentUser' # be aware that LocalMachine requires elevated privileges
+$expirationYears = 1
+
+$SubjectName = $tenant_id + '.' + $client_id
+$cert_password = $client_id
+
+$pfxFileName = $SubjectName + '.pfx'
+$cerFileName = $SubjectName + '.cer'
+
+$PfxFilePath = $FilePath + $pfxFileName
+$CerFilePath = $FilePath + $cerFileName
+
+$CertBeginDate = Get-Date
+$CertExpiryDate = $CertBeginDate.AddYears($expirationYears)
+$SecStringPw = ConvertTo-SecureString -String $cert_password -Force -AsPlainText 
+$Cert = New-SelfSignedCertificate -DnsName $SubjectName -CertStoreLocation "cert:\$StoreLocation\My" -NotBefore $CertBeginDate -NotAfter $CertExpiryDate -KeySpec Signature
+Export-PfxCertificate -cert $Cert -FilePath $PFXFilePath -Password $SecStringPw 
+Export-Certificate -cert $Cert -FilePath $CerFilePath 
---
+++ MSAL_ClientCred_Cert/Program.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.Identity.Client;
+using System;
+using System.Collections.Generic;
+using System.Runtime.ConstrainedExecution;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+
+namespace MSAL_ClientCred_Cert
+{
+
+    /*
+     * Reference: https://blogs.aaddevsup.xyz/2019/01/console_keyvault_clientcredentials/ -- for instructions on how to create a certificate
+     */
+    class Program
+    {
+        static String accessToken = string.Empty; // just to store the access token
+
+        static String client_id = ""; // client id of the app registration you're using -- also used for the cert name and password in this sample
+        static String tenant_id = ""; // tenant id of the tenant -- also used for the cert name in this sample
+        static String cert_password = client_id;
+        static String authority = $"https://login.microsoftonline.com/{tenant_id}";
+        static List<String> scopes = new List<String>() { $"https://graph.microsoft.com/.default" };
+        static String thumbprint = "";
+
+        static void Main( string[] args )
+        {
+
+            Console.WriteLine( "Authenticating..." );
+            GetToken();
+            Console.ReadKey();
+        } 
+
+        static async void GetToken()
+        {
+            accessToken = string.Empty;
+
+            IConfidentialClientApplication ica = ConfidentialClientApplicationBuilder.Create( client_id )
+                .WithCertificate(GetCert( @"C:\Users\raheld\OneDrive - Microsoft\Documents\Certs\"))
+                //.WithCertificate(GetCertByThumbprint( StoreLocation.CurrentUser, thumbprint ) )
+                //.WithCertificate( GetCertByName( StoreLocation.CurrentUser, $"{tenant_id}.{client_id}" ) )
+                .WithTenantId(tenant_id)
+                .WithAuthority(authority)
+                .Build();
+
+            AuthenticationResult authResult = null;
+
+            try
+            {
+                authResult = await ica.AcquireTokenForClient( scopes ).ExecuteAsync();
+                accessToken = authResult.AccessToken;
+
+                Console.WriteLine( $"Access Token: {accessToken}" );
+            } catch ( Exception ex )
+            {
+                Console.WriteLine( $"MSAL Error: {ex.Message}" );
+            }
+
+            
+        }
+
+        /// <summary>
+        /// Gets a certificate by the .pfx name from the file system
+        /// </summary>
+        /// <param name="basePath">The folder where the cert is stored -- filename is derived from the settings in this application</param>
+        /// <returns></returns>
+        static X509Certificate2 GetCert(String basePath)
+        {
+            String certPath = $"{basePath}{tenant_id}.{client_id}.pfx";
+            X509Certificate2 cer = new X509Certificate2( certPath, cert_password, X509KeyStorageFlags.EphemeralKeySet );
+
+            return cer;
+        }
+
+        /// <summary>
+        /// Gets a certificate based on the name -- please note, there is an issue using this method
+        /// if there are multiple certs with the same name.
+        /// </summary>
+        /// <param name="storeLoc"></param>
+        /// <param name="subjectName"></param>
+        /// <returns></returns>
+        static X509Certificate2 GetCertByName(StoreLocation storeLoc, String subjectName )
+        {
+
+            // read certificates already installed from the certificate store
+            X509Store store = new X509Store( storeLoc );
+            X509Certificate2 cer = null;
+
+            store.Open( OpenFlags.ReadOnly );
+
+            // look for the specific cert by subject name -- returns the first one found, if any.  Also, for self signed, set the valid parameter to 'false'
+            X509Certificate2Collection cers = store.Certificates.Find( X509FindType.FindBySubjectName, subjectName, false );
+            if ( cers.Count > 0 )
+            {
+                cer = cers[ 0 ];
+            };
+            store.Close();
+
+            return cer;
+
+        }
+
+        /// <summary>
+        /// Gets a certificate by Thumbprint -- this is the preferred method
+        /// </summary>
+        /// <param name="storeLoc"></param>
+        /// <param name="thumbprint"></param>
+        /// <returns></returns>
+        static X509Certificate2 GetCertByThumbprint(StoreLocation storeLoc, String thumbprint )
+        {
+            X509Store store = new X509Store( storeLoc );
+            X509Certificate2 cer = null;
+
+            store.Open( OpenFlags.ReadOnly );
+
+            // look for the specific cert by thumbprint -- Also, for self signed, set the valid parameter to 'false'
+            X509Certificate2Collection cers = store.Certificates.Find( X509FindType.FindByThumbprint, thumbprint,false );
+            if ( cers.Count > 0 )
+            {
+                cer = cers[ 0 ];
+            };
+            store.Close();
+
+            return cer;
+        }
+    }
+}
